6447fa47e4332a1396e7bb6341a5c702d6e0afb7
Peter <429744889@qq.com>
2019-06-14 09:17:46 +0800

    fix tab and space

--- bank_full/app/main/forms.py
+++ bank_full/app/main/forms.py
@@ -1,16 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, BooleanField, PasswordField, SelectField, TextAreaField, HiddenField
+from wtforms import * 
 from wtforms.validators import DataRequired, Length, Email, Regexp, EqualTo
 
 
 class CfgNotifyForm(FlaskForm):
-    check_order = StringField('排序', validators=[DataRequired(message='不能为空'), Length(0, 64, message='长度不正确')])
-    notify_type = SelectField('通知类型', choices=[('MAIL', '邮件通知'), ('SMS', '短信通知')],
-                              validators=[DataRequired(message='不能为空'), Length(0, 64, message='长度不正确')])
-    notify_name = StringField('通知人姓名', validators=[DataRequired(message='不能为空'), Length(0, 64, message='长度不正确')])
-    notify_number = StringField('通知号码', validators=[DataRequired(message='不能为空'), Length(0, 64, message='长度不正确')])
-    status = BooleanField('生效标识', default=True)
-    submit = SubmitField('提交')
+	check_order = StringField('排序', validators=[DataRequired(message='不能为空'), Length(0, 64, message='长度不正确')])
+	notify_type = SelectField('通知类型', choices=[('MAIL', '邮件通知'), ('SMS', '短信通知')],validators=[DataRequired(message='不能为空'), Length(0, 64, message='长度不正确')])
+	notify_name = StringField('通知人姓名', validators=[DataRequired(message='不能为空'), Length(0, 64, message='长度不正确')])
+	notify_number = StringField('通知号码', validators=[DataRequired(message='不能为空'), Length(0, 64, message='长度不正确')])
+	status = BooleanField('生效标识', default=True)
+	submit = SubmitField('提交')
 
 class BankClass(FlaskForm):
 	bankName = StringField('支行名', validators=[Length(0, 20, message='长度不正确')])
@@ -30,27 +29,27 @@ class account(FlaskForm):
 
 class client(FlaskForm):
 	ID = StringField('ID', validators=[Length(0,20,message='长度不正确')])
-    clientName = StringField('姓名', validators=[Length(0,20,message='长度不正确')])
-    clientPhone = StringField('电话', validators=[Length(0,20,message='长度不正确')])
-    clientAddr = StringField('地址', validators=[Length(0,40,message='长度不正确')])
-    contactName = StringField('联系人姓名', validators=[Length(0,20,message='长度不正确')])
-    contactPhone = StringField('联系人电话', validators=[Length(0,20,message='长度不正确')])
-    contactEmail = StringField('联系人邮件', validators=[Length(0,40,message='长度不正确')])
-    contactRelation = StringField('与本人关系', validators=[Length(0,10,message='长度不正确')])
+	clientName = StringField('姓名', validators=[Length(0,20,message='长度不正确')])
+	clientPhone = StringField('电话', validators=[Length(0,20,message='长度不正确')])
+	clientAddr = StringField('地址', validators=[Length(0,40,message='长度不正确')])
+	contactName = StringField('联系人姓名', validators=[Length(0,20,message='长度不正确')])
+	contactPhone = StringField('联系人电话', validators=[Length(0,20,message='长度不正确')])
+	contactEmail = StringField('联系人邮件', validators=[Length(0,40,message='长度不正确')])
+	contactRelation = StringField('与本人关系', validators=[Length(0,10,message='长度不正确')])
 
 class loans(FlaskForm):
 	branchName = StringField('支行', validators=[Length(0,20,message='长度不正确')])
-    loanId = StringField('ID', validators=[Length(0,6,message='长度不正确')])
-    loanAmount = FloatField('数额', validators=[Length(0, 20, message='长度不正确')])
-    payNum = IntegerField('逐次支付', validators=[Length(0,20,message='长度不正确')])
+	loanId = StringField('ID', validators=[Length(0,6,message='长度不正确')])
+	loanAmount = FloatField('数额', validators=[Length(0, 20, message='长度不正确')])
+	payNum = IntegerField('逐次支付', validators=[Length(0,20,message='长度不正确')])
 
 
 class grant(FlaskForm):
 	branchName = StringField('支行', validators=[Length(0,20,message='长度不正确')])
-    loanId = StringField('支行', validators=[Length(0,6,message='长度不正确')])
-    grantCount = IntegerField('次数', validators=[Length(0,20,message='长度不正确')])
-    grantTime = DateTimeField('支付时间')
-    grantMoney = FloatField('金额', validators=[Length(0, 20, message='长度不正确')])
+	loanId = StringField('支行', validators=[Length(0,6,message='长度不正确')])
+	grantCount = IntegerField('次数', validators=[Length(0,20,message='长度不正确')])
+	grantTime = DateTimeField('支付时间')
+	grantMoney = FloatField('金额', validators=[Length(0, 20, message='长度不正确')])
 
 
 
