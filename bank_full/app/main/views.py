ac83f689d8772bb98ce7b2a7ce7d63a3fbc2a5ce
unknown <rg1336@mail.ustc.edu.cn>
2019-06-14 16:30:57 +0800

    添加了注释 供后来人评价

--- bank_full/app/main/views.py
+++ bank_full/app/main/views.py
@@ -41,8 +41,6 @@ def common_list(DynamicModel,form,view):
     dict = {'content': query_to_list(query), 'total_count': total_count,
             'total_page': math.ceil(total_count / length), 'page': page, 'length': length}
     return render_template(view, form=dict,form_search = form,current_user=current_user)
-
-
 # 通用单模型查询&新增&修改
 def common_edit(DynamicModel, form, view):
     id = request.args.get('id', '')
@@ -88,27 +86,25 @@ def index():
     return render_template('index.html', current_user=current_user)
 
 
-# 支行查询
+#========== 支行查询和相关管理==================
+#++++++++++++++++++++++++++++这里是我复写的两个common方法，东西是共同的，但是主码的区别导致所有的方法必须被分开，不然没法用
 def common_list_bank(DynamicModel,form,view):
     # 接收参数
-    action = request.args.get('action')
-    del_name = request.args.get('id')
-    print("AAAAAAAAAAAAAAAAAAAAAAAAA")
-    print(action)
-    print(del_name)
+    del_name = request.args.get('id')   ####从html代码中得到删除的相关信息
     model = DynamicModel()
     form_to_model(form, model)
-    dct = model_to_dict(model)
+    dct = model_to_dict(model) ####将model转化为dict，方便输出debug和输入
     name = dct['branchName']
     city = dct['branchCity']
     print(dct)
-    if del_name !=None:
-        print("HERE")
+    if del_name !=None:   ####严重问题：由于我没有看懂html代码，因此这里每次执行删除操作后似乎没法继续查找，只有退出再进来才行
+        ######################总之就是显示的非常混乱就对了
         try:
             DynamicModel.get(DynamicModel.branchName == del_name).delete_instance()
             flash('删除成功')
         except:
             flash('删除失败')
+    ####根据不同的现有数据进行查询，接下来的一些实现中可以放宽需求。
     if len(name) and len(city)  :
         query = DynamicModel.select().where((DynamicModel.branchName == name) & (DynamicModel.branchCity == city))
     elif len(name):
@@ -118,8 +114,7 @@ def common_list_bank(DynamicModel,form,view):
     else:
         query = DynamicModel.select()
     total_count = query.count()
-    print("BBBBBBBBBBBB")
-    print(total_count)
+    #####重新组织好查询结果
     list = []
     for e in query:
         list.append(model_to_dict(e))
@@ -136,31 +131,30 @@ def common_edit_bank(DynamicModel, form, view):
         name = dct['branchName']
         if DynamicModel.select().where(DynamicModel.branchName == name).count():  ###这一句话的含义是，通过查看对应的主码来判断是否存在对应的条目
             ####有了
-            print("YESSSSSSSS")
             model.save()
             flash('修改成功')
         else:
             ###莫得
-            print("NOOOOOOOOO")
             DynamicModel.insert(dct).execute()
             flash('保存成功')
     else:
         flash_errors(form)
     return render_template(view, form=form, current_user=current_user)
    
-
 @main.route('/notifylist_bank', methods=['GET', 'POST'])
 @login_required
 def notifylist_bank():
     return common_list_bank(Branch, Bank(), 'notifylist_bank.html')
-
-
+    #######################################################对于html页面，删除与分页相关的东西就可以了，对比一下能看出来
 # 支行管理
+
 @main.route('/notifyedit_bank', methods=['GET', 'POST'])
 @login_required
 def notifyedit_bank():
     return common_edit_bank(Branch, Bank(), 'notifyedit_bank.html')
 
+
+# ============员工管理部分============
 # stuff
 @main.route('/notifylist_stuff', methods=['GET', 'POST'])
 @login_required
@@ -174,7 +168,9 @@ def notifylist_stuff():
 def notifyedit_stuff():
     return common_edit(Staff,staff(), 'notifyedit_stuff.html')
 
-# client
+
+
+# ==================用户管理部分=========
 @main.route('/notifylist_client', methods=['GET', 'POST'])
 @login_required
 def notifylist_client():
@@ -185,7 +181,8 @@ def notifylist_client():
 def notifyedit_client():
     return common_edit(Client, client(), 'notifyedit_client.html')
 
-# account
+
+#==================账户管理部分================
 @main.route('/notifylist_account', methods=['GET', 'POST'])
 @login_required
 def notifylist_account():
@@ -197,7 +194,7 @@ def notifylist_account():
 def notifyedit_account():
     return common_edit(CfgNotify, CfgNotifyForm(), 'notifyedit_account.html')
 
-# loan
+#====================贷款管理===================
 @main.route('/notifylist_loans', methods=['GET', 'POST'])
 @login_required
 def notifylist_loans():
